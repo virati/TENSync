4ace5246f792ca763cefd2cd8f197c752d207c70
Vineet Tiruvadi <virati@veritexx.net>
2015-02-25 14:45:29 -0500

    changes to analog read, allowing for better logging and etc.

--- analog_read.py
+++ analog_read.py
@@ -12,14 +12,14 @@ trigger_counter = 0
 
 fo = open("DBS903_TENS_Trigger.txt","wb")
 
-while trigger_counter < 2:
+while trigger_counter < 5:
 	voltage = ADC.read("P9_39")*1.8
 	#print(voltage)
 
 	if voltage < 1.00:
 		current_time = datetime.datetime.now().time()
 		log.append(current_time)
-		print('TRIGGER ' + str(voltage))
+		print('TRIGGER @' + str(voltage) + 'V; Trigger NUMBER: ' + str(trigger_counter))
 		fo.write(str(current_time) + "\n")
 		trigger_counter += 1
 
